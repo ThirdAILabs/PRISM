98684803862794f27da3588cf36d4a223e43beb9
Nicholas Meisburger <nmeisburger@gmail.com>
2025-02-06 11:54:40 -0500

    add flash bindings

---
+++ prism/search/flash_test.go
@@ -0,0 +1,51 @@
+package search_test
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"prism/search"
+	"strings"
+	"testing"
+)
+
+func init() {
+	const licensePath = "../../.test_license/thirdai.license"
+	if err := search.SetLicensePath(licensePath); err != nil {
+		panic(err)
+	}
+}
+
+func TestFlash(t *testing.T) {
+	flash, err := search.NewFlash()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer flash.Free()
+
+	data := []string{"entity", "this is a first sample", "completely different text here", "apples are a good fruit", "hopefully this is enough data for the test"}
+
+	filename := filepath.Join(t.TempDir(), "test.csv")
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := io.Copy(file, strings.NewReader(strings.Join(data, "\n"))); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := flash.Train(filename); err != nil {
+		t.Fatal(err)
+	}
+
+	results, err := flash.Predict("this is enough", 3)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(results) < 1 || results[0] != data[4] {
+		t.Fatalf("incorrect results: %v", results)
+	}
+}
---
+++ prism/search/include/Flash.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <algorithm>
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <memory>
+
+namespace thirdai::automl::udt {
+
+class Flash {
+public:
+  static std::unique_ptr<Flash>
+  make(const std::optional<std::string> &input_column,
+       const std::string &target_column, const std::string &dataset_size);
+
+  void trainOnFile(const std::string &filename);
+
+  std::vector<std::string> predictSimple(const std::string &sample,
+                                         uint32_t top_k);
+};
+
+} // namespace thirdai::automl::udt
