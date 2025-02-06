96c77d1418b65e6467762c29901a8aefe1aa5e9d
Nicholas Meisburger <nmeisburger@gmail.com>
2025-02-05 21:42:57 -0500

    complete acknowledgement extractor

--- prism/go.mod
+++ prism/go.mod
@@ -4,28 +4,34 @@ go 1.23.2
 
 require (
 	github.com/Nerzal/gocloak/v13 v13.9.0
+	github.com/PuerkitoBio/goquery v1.10.1
+	github.com/agnivade/levenshtein v1.2.1
 	github.com/go-chi/chi/v5 v5.2.0
 	github.com/google/uuid v1.6.0
 	github.com/openai/openai-go v0.1.0-alpha.50
+	github.com/playwright-community/playwright-go v0.4902.0
+	go.etcd.io/bbolt v1.4.0
 	golang.org/x/crypto v0.32.0
 	gorm.io/gorm v1.25.12
 )
 
 require (
-	github.com/agnivade/levenshtein v1.2.1 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/deckarep/golang-set/v2 v2.7.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
 	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/segmentio/ksuid v1.0.4 // indirect
-	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/gjson v1.17.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/tidwall/sjson v1.2.5 // indirect
-	go.etcd.io/bbolt v1.4.0 // indirect
-	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 )
--- prism/reports/flaggers/grobid.go
+++ prism/reports/flaggers/grobid.go
@@ -1,26 +1,39 @@
 package flaggers
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"log/slog"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
 	"prism/openalex"
 	"strings"
+	"time"
 
+	"github.com/PuerkitoBio/goquery"
+	"github.com/google/uuid"
+	"github.com/playwright-community/playwright-go"
 	"go.etcd.io/bbolt"
 )
 
 var cacheBucket = []byte("ack-cache")
 
 type AcknowledgementsExtractor struct {
-	cache      *bbolt.DB
-	maxWorkers int
+	cache          *bbolt.DB
+	maxWorkers     int
+	grobidEndpoint string
+	downloadDir    string
 }
 
 type Entity struct {
-	Entity   string
-	Type     string
-	Position int
+	EntityText    string
+	EntityType    string
+	StartPosition int
 }
 
 type Acknowledgement struct {
@@ -76,7 +89,7 @@ func (extractor *AcknowledgementsExtractor) worker(queue chan openalex.Work, out
 
 		id := parseOpenAlexId(next)
 
-		acks, err := extractor.extractAcknowledgments(next)
+		acks, err := extractor.extractAcknowledgments(id, next)
 		if err != nil {
 			slog.Error("error extracting acknowledgements for work", "id", id, "name", next.DisplayName, "error", err)
 			errorCh <- fmt.Errorf("error extracting acknowledgments: %w", err)
@@ -146,6 +159,204 @@ func (extractor *AcknowledgementsExtractor) updateCache(id string, acks Acknowle
 	slog.Info("successfully updated acknowledgements cache", "id", id)
 }
 
-func (extractor *AcknowledgementsExtractor) extractAcknowledgments(work openalex.Work) (Acknowledgements, error) {
+func (extractor *AcknowledgementsExtractor) extractAcknowledgments(id string, work openalex.Work) (Acknowledgements, error) {
+	slog.Info("extracting acknowledgments from", "id", work.WorkId, "name", work.DisplayName)
 
+	destPath := filepath.Join(extractor.downloadDir, uuid.NewString()+".pdf")
+	pdf, err := downloadPdf(work.DownloadUrl, destPath)
+	if err != nil {
+		slog.Error("error downloading pdf", "id", work.WorkId, "name", work.DisplayName, "error", err)
+		return Acknowledgements{}, err
+	}
+	defer pdf.Close()
+
+	defer func() {
+		if err := os.Remove(destPath); err != nil && !errors.Is(err, os.ErrNotExist) {
+			slog.Error("error removing temp download file", "error", err)
+		}
+
+	}()
+
+	acks, err := extractor.processPdfWithGrobid(pdf)
+	if err != nil {
+		slog.Error("error processing pdf with grobid", "id", work.WorkId, "name", work.DisplayName, "error", err)
+		return Acknowledgements{}, err
+	}
+
+	return Acknowledgements{OpenAlexId: id, Acknowledgements: acks}, nil
+}
+
+func downloadWithPlaywright(url, destPath string) (io.ReadCloser, error) {
+	pw, err := playwright.Run(&playwright.RunOptions{Browsers: []string{"firefox"}})
+	if err != nil {
+		return nil, fmt.Errorf("error starting playwright: %w", err)
+	}
+	defer pw.Stop()
+
+	browser, err := pw.Firefox.Launch()
+	if err != nil {
+		return nil, fmt.Errorf("error launching browser: %w", err)
+	}
+	defer browser.Close()
+
+	context, err := browser.NewContext(playwright.BrowserNewContextOptions{
+		AcceptDownloads:   playwright.Bool(true),
+		IgnoreHttpsErrors: playwright.Bool(true),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error creating browser context: %w", err)
+	}
+	defer context.Close()
+
+	page, err := context.NewPage()
+	if err != nil {
+		return nil, fmt.Errorf("error opening browser page: %w", err)
+	}
+	// context.Close() closes pages in the context
+
+	download, err := page.ExpectDownload(nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating download handler: %w", err)
+	}
+
+	if _, err := page.Goto(url, playwright.PageGotoOptions{WaitUntil: playwright.WaitUntilStateNetworkidle}); err != nil {
+		return nil, fmt.Errorf("error accessing download url: %w", err)
+	}
+
+	if err := download.SaveAs(destPath); err != nil {
+		return nil, fmt.Errorf("error downloading paper: %w", err)
+	}
+
+	file, err := os.Open(destPath)
+	if err != nil {
+		return nil, fmt.Errorf("error opening downloaded file: %w", err)
+	}
+
+	return file, nil
+}
+
+var headers = map[string]string{
+	"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+	"accept-language":           "en-US,en;q=0.9",
+	"cache-control":             "max-age=0",
+	"user-agent":                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+	"upgrade-insecure-requests": "1",
+	"sec-ch-ua":                 `"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"`,
+	"sec-ch-ua-mobile":          "?0",
+	"sec-ch-ua-platform":        `"Windows"`,
+}
+
+func downloadWithHttp(url string) (io.ReadCloser, error) {
+	req, err := http.NewRequest("GET", strings.Replace(url, " ", "%20", -1), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating http request: %w", err)
+	}
+
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error downloading pdf: %w", err)
+	}
+
+	return res.Body, nil
+}
+
+func downloadPdf(url, destPath string) (io.ReadCloser, error) {
+	slog.Info("attempting to download pdf", "url", url)
+
+	attempt1, err1 := downloadWithHttp(url)
+	if err1 != nil {
+		slog.Error("basic download failed", "url", url, "error", err1)
+	} else {
+		return attempt1, nil
+	}
+
+	attempt2, err2 := downloadWithPlaywright(url, destPath)
+	if err2 != nil {
+		slog.Error("playwright download failed", "url", url, "error", err2)
+	} else {
+		return attempt2, nil
+	}
+
+	return nil, fmt.Errorf("unable to download pdf:\n%w\n%w", err1, err2)
+}
+
+var searchAbleEntityTypes = map[string]bool{
+	"affiliation": true,
+	"funderName":  true,
+	"grantName":   true,
+	"institution": true,
+	"programName": true,
+	"projectName": true,
+	"funder":      true,
+}
+
+func parseGrobidReponse(data io.Reader) ([]Acknowledgement, error) {
+	doc, err := goquery.NewDocumentFromReader(data)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing grobid response: %w", err)
+	}
+
+	acks := make([]Acknowledgement, 0)
+
+	processor := func(i int, s *goquery.Selection) {
+		text := strings.TrimSpace(s.Text())
+
+		searchable := make([]Entity, 0)
+		misc := make([]Entity, 0)
+
+		last := 0
+
+		s.Find("rs").Each(func(i int, s *goquery.Selection) {
+			entityText := s.Text()
+			entityType := s.AttrOr("type", "misc")
+			start := strings.Index(text[last:], entityText)
+			last = start + len(entityText)
+
+			if searchAbleEntityTypes[entityType] {
+				searchable = append(searchable, Entity{EntityText: entityText, EntityType: entityType, StartPosition: start})
+			} else {
+				misc = append(misc, Entity{EntityText: entityText, EntityType: entityType, StartPosition: start})
+			}
+		})
+
+		acks = append(acks, Acknowledgement{RawText: text, SearchableEntities: searchable, MiscEntities: misc})
+	}
+
+	doc.Find("div[type=acknowledgement]").Each(processor)
+	doc.Find("div[type=funding]").Each(processor)
+
+	return acks, nil
+}
+
+func (extractor *AcknowledgementsExtractor) processPdfWithGrobid(pdf io.Reader) ([]Acknowledgement, error) {
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+
+	part, err := writer.CreateFormFile("input", "filename.pdf")
+	if err != nil {
+		return nil, fmt.Errorf("error creating multipart request: %w", err)
+	}
+
+	if _, err := io.Copy(part, pdf); err != nil {
+		return nil, fmt.Errorf("error copying data to multipart request: %w", err)
+	}
+
+	if err := writer.Close(); err != nil {
+		return nil, fmt.Errorf("error finalizing multipart request: %w", err)
+	}
+
+	// TODO: add backoff/retry
+	res, err := http.Post(extractor.grobidEndpoint, writer.FormDataContentType(), body)
+	if err != nil {
+		return nil, fmt.Errorf("error making request to grobid: %w", err)
+	}
+	defer res.Body.Close()
+
+	return parseGrobidReponse(res.Body)
 }
