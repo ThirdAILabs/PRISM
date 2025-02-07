ac2663ffc6d1f7d1397d92551b4b8a1c7065eb09
Anand Kumar <anand@thirdai.com>
2025-02-07 14:21:21 +0530

    Copied frontend files form Rice-research-security to PRISM

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SearchComponent from './SearchComponent';
+import ItemDetails from './ItemDetails';
+import EntityLookup from './EntityLookup';
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<SearchComponent />} />
+          <Route path="/item" element={<ItemDetails />} />
+          <Route path="/entity-lookup" element={<EntityLookup />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/constants.js
@@ -0,0 +1,10 @@
+export const MULTI_AFFIL = "oa_multi_affil";
+export const FUNDER_EOC = "oa_funder_eoc";
+export const PUBLISHER_EOC = "oa_publisher_eoc";    // isn't used   
+export const COAUTHOR_EOC = "oa_coauthor_eoc";
+export const COAUTHOR_AFFIL_EOC = "oa_coauthor_affiliation_eoc";
+export const AUTHOR_AFFIL_EOC = "oa_author_affiliation_eoc";
+export const ACK_EOC = "oa_acknowledgement_eoc";
+export const FORMAL_RELATIONS = "formal_relations";
+export const UNI_FACULTY_EOC = "uni_faculty_eoc";
+export const DOJ_PRESS_RELEASES_EOC = "doj_press_release_eoc"
