4e8ac36f8308cb7cc68bcc78b636ed796fa4d58e
Yash <yash@thirdai.com>
2025-02-25 16:00:29 -0600

    Format Check (#31)

--- frontend/src/components/common/tools/Tabs.js
+++ frontend/src/components/common/tools/Tabs.js
@@ -4,79 +4,83 @@ import { Tabs, Tab, Box, Divider } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
-    tabs: {
-        '& .MuiTabs-indicator': {
-            backgroundColor: 'black',
-            height: '3px', // Makes the active tab indicator bolder
-        },
+  tabs: {
+    '& .MuiTabs-indicator': {
+      backgroundColor: 'black',
+      height: '3px', // Makes the active tab indicator bolder
     },
-    tab: {
-        color: 'black !important',
-        '&.Mui-selected': {
-            color: 'black !important',
-        },
-        '&:hover': {
-            backgroundColor: 'rgb(245,240,240) !important', // Light gray hover effect
-            transition: 'background-color 0.3s',
-        },
+  },
+  tab: {
+    color: 'black !important',
+    '&.Mui-selected': {
+      color: 'black !important',
     },
+    '&:hover': {
+      backgroundColor: 'rgb(245,240,240) !important', // Light gray hover effect
+      transition: 'background-color 0.3s',
+    },
+  },
 });
 
 const CustomTabs = ({ activeTab, handleTabChange }) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Box>
-            <Box sx={{
-                display: 'flex',
-                justifyContent: 'center', // Center the Tabs horizontally
-                borderBottom: 0
-            }}>
-                <Tabs
-                    value={activeTab}
-                    onChange={handleTabChange}
-                    className={classes.tabs}
-                    TabIndicatorProps={{
-                        style: {
-                            backgroundColor: 'black'
-                        }
-                    }}
-                    sx={{
-                        '& .MuiTabs-flexContainer': {
-                            gap: '2rem'
-                        },
-                        minHeight: '48px'
-                    }}
-                >
-                    <Tab
-                        label="Dashboard"
-                        className={classes.tab}
-                        sx={{
-                            textTransform: 'none',
-                            minHeight: '48px',
-                            padding: '12px 16px'
-                        }}
-                    />
-                    <Tab
-                        label="Graph Visualization"
-                        className={classes.tab}
-                        sx={{
-                            textTransform: 'none',
-                            minHeight: '48px',
-                            padding: '12px 16px'
-                        }}
-                    />
-                </Tabs>
-            </Box>
-            {/* Added white divider below tabs */}
-            <Divider sx={{
-                backgroundColor: 'black',
-                height: '1px',
-                width: '100%',
-                opacity: 0.1
-            }} />
-        </Box>
-    );
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center', // Center the Tabs horizontally
+          borderBottom: 0,
+        }}
+      >
+        <Tabs
+          value={activeTab}
+          onChange={handleTabChange}
+          className={classes.tabs}
+          TabIndicatorProps={{
+            style: {
+              backgroundColor: 'black',
+            },
+          }}
+          sx={{
+            '& .MuiTabs-flexContainer': {
+              gap: '2rem',
+            },
+            minHeight: '48px',
+          }}
+        >
+          <Tab
+            label="Dashboard"
+            className={classes.tab}
+            sx={{
+              textTransform: 'none',
+              minHeight: '48px',
+              padding: '12px 16px',
+            }}
+          />
+          <Tab
+            label="Graph Visualization"
+            className={classes.tab}
+            sx={{
+              textTransform: 'none',
+              minHeight: '48px',
+              padding: '12px 16px',
+            }}
+          />
+        </Tabs>
+      </Box>
+      {/* Added white divider below tabs */}
+      <Divider
+        sx={{
+          backgroundColor: 'black',
+          height: '1px',
+          width: '100%',
+          opacity: 0.1,
+        }}
+      />
+    </Box>
+  );
 };
 
 export default CustomTabs;
--- frontend/src/constants/eoc_categories.js
+++ frontend/src/constants/eoc_categories.js
@@ -1,16 +1,10 @@
 export const talentPrograms = [
-    'Department of Defense - Foreign Talent Programs that Pose a Threat to National Security Interests of the United States',
-    'Foreign Talent Recruitment Programs'
+  'Department of Defense - Foreign Talent Programs that Pose a Threat to National Security Interests of the United States',
+  'Foreign Talent Recruitment Programs',
 ];
 
-export const deniedEntities = [
-    'Chinese Military Companies Operating in the United States'
-];
+export const deniedEntities = ['Chinese Military Companies Operating in the United States'];
 
-export const fundingSources = [
-    'Department of Defense - Entities of Concern'
-];
+export const fundingSources = ['Department of Defense - Entities of Concern'];
 
-export const institutions = [
-    'Department of Defense - Entities of Concern'
-]
+export const institutions = ['Department of Defense - Entities of Concern'];
