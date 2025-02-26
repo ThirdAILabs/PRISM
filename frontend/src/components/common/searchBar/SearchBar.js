26c9b1f5f5da7cc21145aed7f4e6597da97c89c6
Anand Kumar <anand@thirdai.com>
2025-02-27 03:27:40 +0530

    autocomplete function in Utils, University assessment page

--- frontend/src/components/common/searchBar/SearchBar.js
+++ frontend/src/components/common/searchBar/SearchBar.js
@@ -3,65 +3,65 @@ import { autocompleteService } from '../../../api/autocomplete';
 import './SearchBar.css';
 import '../tools/button/button1.css';
 import useCallOnPause from '../../../hooks/useCallOnPause';
+import AutocompleteSearchBar from '../../../utils/autocomplete';
+// function AutocompleteSearchBar({ title, autocomplete, onSelect, type }) {
+//   const [suggestions, setSuggestions] = useState([]);
+//   const [query, setQuery] = useState('');
 
-function AutocompleteSearchBar({ title, autocomplete, onSelect, type }) {
-  const [suggestions, setSuggestions] = useState([]);
-  const [query, setQuery] = useState('');
+//   function handleInputChange(e) {
+//     setQuery(e.target.value);
+//     autocomplete(e.target.value).then(setSuggestions);
+//   }
 
-  function handleInputChange(e) {
-    setQuery(e.target.value);
-    autocomplete(e.target.value).then(setSuggestions);
-  }
+//   function handleSelectSuggestion(suggestion) {
+//     return () => {
+//       setSuggestions([]);
+//       setQuery(type === 'author' ? suggestion.AuthorName : suggestion.InstitutionName);
+//       onSelect(suggestion);
+//     };
+//   }
 
-  function handleSelectSuggestion(suggestion) {
-    return () => {
-      setSuggestions([]);
-      setQuery(type === 'author' ? suggestion.AuthorName : suggestion.InstitutionName);
-      onSelect(suggestion);
-    };
-  }
+//   function handleSelectSuggestionInstitute(suggestion) {
+//     return () => {
+//       setSuggestions([]);
+//       setQuery(suggestion.InstitutionName);
+//       onSelect(suggestion);
+//     };
+//   }
 
-  function handleSelectSuggestionInstitute(suggestion) {
-    return () => {
-      setSuggestions([]);
-      setQuery(suggestion.InstitutionName);
-      onSelect(suggestion);
-    };
-  }
+//   return (
+//     // Column
+//     <div className="autocomplete-search-bar">
+//       {/* Header 2 or 3, bold */}
+//       <div className="autocomplete-search-bar-title">{title}</div>
 
-  return (
-    // Column
-    <div className="autocomplete-search-bar">
-      {/* Header 2 or 3, bold */}
-      <div className="autocomplete-search-bar-title">{title}</div>
+//       {/* Search bar */}
+//       <input type="text" className="search-bar" value={query} onChange={handleInputChange} />
 
-      {/* Search bar */}
-      <input type="text" className="search-bar" value={query} onChange={handleInputChange} />
-
-      {query && query.length && suggestions && suggestions.length > 0 && (
-        // Autocomplete suggestion container. Column.
-        <div className="suggestion-container">
-          {suggestions.map((suggestion, index) =>
-            // Clickable suggestion
-            type === 'author' ? (
-              <div className="suggestion" key={index} onClick={handleSelectSuggestion(suggestion)}>
-                {suggestion.AuthorName}
-              </div>
-            ) : (
-              <div
-                className="suggestion"
-                key={index}
-                onClick={handleSelectSuggestionInstitute(suggestion)}
-              >
-                {suggestion.InstitutionName}
-              </div>
-            )
-          )}
-        </div>
-      )}
-    </div>
-  );
-}
+//       {query && query.length && suggestions && suggestions.length > 0 && (
+//         // Autocomplete suggestion container. Column.
+//         <div className="suggestion-container">
+//           {suggestions.map((suggestion, index) =>
+//             // Clickable suggestion
+//             type === 'author' ? (
+//               <div className="suggestion" key={index} onClick={handleSelectSuggestion(suggestion)}>
+//                 {suggestion.AuthorName}
+//               </div>
+//             ) : (
+//               <div
+//                 className="suggestion"
+//                 key={index}
+//                 onClick={handleSelectSuggestionInstitute(suggestion)}
+//               >
+//                 {suggestion.InstitutionName}
+//               </div>
+//             )
+//           )}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
 export function AuthorInstiutionSearchBar({ onSearch }) {
   const [author, setAuthor] = useState();
--- frontend/src/components/pages/UniversityAssessment.js
+++ frontend/src/components/pages/UniversityAssessment.js
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import Logo from '../../assets/images/prism-logo.png';
+import '../common/searchBar/SearchBar.css';
+import '../common/tools/button/button1.css';
+import { searchService } from '../../api/search';
+import AutocompleteSearchBar from '../../utils/autocomplete';
+import { autocompleteService } from '../../api/autocomplete';
+import useCallOnPause from '../../hooks/useCallOnPause';
 
-const UniversityAssessment = () => {
+function UniversityAssessment() {
+  const [institution, setInstitution] = useState();
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const debouncedSearch = useCallOnPause(300); // 300ms delay
+
+  const autocompleteInstitution = useCallback(
+    (query) => {
+      return new Promise((resolve) => {
+        debouncedSearch(async () => {
+          try {
+            const res = await autocompleteService.autocompleteInstitutions(query);
+            resolve(res);
+            return res;
+          } catch (error) {
+            console.error('Autocomplete error:', error);
+            resolve([]);
+          }
+        });
+      });
+    },
+    [debouncedSearch]
+  );
   return (
-    <div>
-      <h1>University Assessment</h1>
+    <div className="basic-setup" style={{ color: 'white' }}>
+      <div style={{ textAlign: 'center', marginTop: '5.5%', animation: 'fade-in 0.75s' }}>
+        <img
+          src={Logo}
+          style={{
+            width: '320px',
+            marginTop: '5%',
+            marginBottom: '1%',
+            marginRight: '2%',
+            animation: 'fade-in 0.5s',
+          }}
+        />
+        <div style={{ animation: 'fade-in 1s' }}>
+          <div className="d-flex justify-content-center align-items-center">
+            <div style={{ marginTop: 10, color: '#888888' }}>
+              We help you comply with research security requirements by automating university assessments.
+            </div>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <div style={{ marginTop: 10, marginBottom: '2%', color: '#888888' }}>
+              Who would you like to conduct an assessment on?
+            </div>
+          </div>
+        </div>
+        <div style={{ animation: 'fade-in 1s' }}>
+          <div className="d-flex justify-content-center align-items-center pt-5">
+            <div style={{ width: '80%' }}>
+              <div className="author-institution-search-bar">
+                <div className="autocomplete-search-bar">
+                  <AutocompleteSearchBar
+                    autocomplete={autocompleteInstitution}
+                    onSelect={setInstitution}
+                    type={'institute'}
+                  />
+                </div>
+                <div style={{ width: '20px' }} />
+                <div style={{ width: '200px' }}>
+                  <button className="button">
+                    {isLoading ? 'Searching...' : 'Search'}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default UniversityAssessment;
+
