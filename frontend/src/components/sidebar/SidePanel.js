9786671488b7fbab8b5c4335d5fb519c01c657be
Anand Kumar <anand@thirdai.com>
2025-02-27 01:12:56 +0530

    improved sidepanel

--- frontend/src/components/sidebar/SidePanel.js
+++ frontend/src/components/sidebar/SidePanel.js
@@ -2,24 +2,28 @@ import { useEffect, useState } from 'react';
 import { reportService } from '../../api/reports';
 import './Sidepanel.css';
 import RandomAvatar from '../../assets/images/RandomAvatar.jpg'
-// import PRISM_LOGO from '../../assets/icons/prism-logo.svg';
 import PRISM_LOGO from '../../assets/images/prism.png';
-import { FaBars, FaRegUserCircle, FaUniversity, FaSearch } from 'react-icons/fa';
+import { FaRegUserCircle, FaUniversity, FaSearch } from 'react-icons/fa';
 import UserService from '../../services/userService';
 import { CiLogout } from "react-icons/ci";
 import { TbReportSearch } from "react-icons/tb";
 import { CiCircleList, CiCircleCheck } from "react-icons/ci";
 import { CgSpinner } from "react-icons/cg";
 import { useNavigate } from 'react-router-dom';
+import { useUser } from '../../store/userContext';
+import { MdDelete } from "react-icons/md";
+import { useLocation } from 'react-router-dom';
 
 
 const SidePanel = ({ isOpen, onClose }) => {
+    const { userInfo } = useUser();
     const [reports, setReports] = useState([]);
     const navigate = useNavigate();
     const user = {
         avatar: RandomAvatar,
-        name: 'Anand Kumar',
-        // email: 'admin@mail.com'
+        name: userInfo.name,
+        email: userInfo.email,
+        username: userInfo.username,
     };
 
     const status = {
@@ -27,6 +31,8 @@ const SidePanel = ({ isOpen, onClose }) => {
         'in-progress': <CgSpinner className="animate-spin" title="In-Progress" size={20} />,
         'complete': <CiCircleCheck title="Completed" size={20} />
     };
+    const location = useLocation();
+    const currentLocation = location.pathname;
 
     useEffect(() => {
         const fetchReports = async () => {
@@ -35,6 +41,7 @@ const SidePanel = ({ isOpen, onClose }) => {
         };
         fetchReports();
     }, []);
+
     const handleEntityClick = () => {
         onClose();
         navigate('/entity-lookup');
@@ -52,36 +59,39 @@ const SidePanel = ({ isOpen, onClose }) => {
         navigate(`/report/${report.Id
             }`);
     };
+
     return (
         <div className={`side-panel ${isOpen ? 'open' : ''}`}>
             <div className="panel-content">
-                <div style={{ display: 'flex', alignItems: 'center', padding: '10px 20px', paddingTop: '0', borderBottom: '1px solid #e0e0e0', gap: '20px' }}>
+                {/* Header */}
+                <div className="side-panel-header" style={{ display: 'flex', alignItems: 'center', padding: '10px 20px', paddingTop: '0', borderBottom: '1px solid #e0e0e0', gap: '20px' }}>
                     <img src={PRISM_LOGO} alt="PRISM" style={{ width: "150px", height: '30px', marginLeft: '10%' }} />
                 </div>
-
-
+                {/* Navigation */}
                 <nav className="navigation">
                     <ul className="nav-list">
-                        <li className="nav-item" onClick={handleIndividualClick}>
+                        <li className={`nav-item ${currentLocation === '/' ? 'active' : ''}`} onClick={handleIndividualClick}>
                             <span className="nav-icon"><FaRegUserCircle /></span>
                             <span className="nav-text">Individual Assessment</span>
                         </li>
-                        <li className="nav-item" onClick={handleUniversityClick}>
+                        <li className={`nav-item ${currentLocation === '/university-assessment' ? 'active' : ''}`} onClick={handleUniversityClick}>
                             <span className="nav-icon"><FaUniversity /></span>
                             <span className="nav-text">University Assessment</span>
                         </li>
-                        <li className="nav-item" onClick={handleEntityClick}>
+                        <li className={`nav-item ${currentLocation === '/entity-lookup' ? 'active' : ''}`} onClick={handleEntityClick}>
                             <span className="nav-icon"><FaSearch /></span>
                             <span className="nav-text">Entity Lookup</span>
                         </li>
                     </ul>
                 </nav>
 
+                {/* Reports */}
                 <div className="reports">
                     <h5 className='report-header'><TbReportSearch /> Reports</h5>
-                    {reports.map(report => (
-                        <div key={report.Id} className="report-item" onClick={handleReportClick.bind(null, report)}>
+                    {reports.map((report, index) => (
+                        (index < 10) && <div key={report.Id} className="report-item" onClick={handleReportClick.bind(null, report)}>
                             <p>{report.AuthorName}</p>
+                            {/* <span><MdDelete style={15} /></span> */}
                             <span className={`status ${report.Status}`}>{status[report.Status]}</span>
                         </div>
                     ))}
@@ -93,7 +103,7 @@ const SidePanel = ({ isOpen, onClose }) => {
             <div className="user-info">
                 <img src={user.avatar} alt="User" style={{ width: "40px", height: '40px', borderRadius: '100%' }} />
                 <div>
-                    <h5>{user.name}</h5>
+                    <h5>{user.username}</h5>
                 </div>
             </div>
 
