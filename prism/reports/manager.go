ccc2cf3f22b821b4cf4d39327a4bc39fc7552290
Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
2025-02-27 15:56:41 -0500

    Prioritize individual reports over university reports (#45)

--- prism/reports/manager.go
+++ prism/reports/manager.go
@@ -59,10 +59,13 @@ func (r *ReportManager) ListAuthorReports(userId uuid.UUID) ([]api.Report, error
 	return results, nil
 }
 
+// This function is only called from GetAuthorReport or CreateAuthorReport. The university reports
+// use a different methods that can check all the author reports associated with the university report
+// at once.
 func (r *ReportManager) queueAuthorReportUpdateIfNeeded(txn *gorm.DB, report *schema.AuthorReport) error {
 	if time.Now().UTC().Sub(report.LastUpdatedAt) > r.staleReportThreshold &&
 		report.Status != schema.ReportInProgress && report.Status != schema.ReportQueued {
-		updates := map[string]any{"status": schema.ReportQueued, "queued_at": time.Now().UTC()}
+		updates := map[string]any{"status": schema.ReportQueued, "queued_at": time.Now().UTC(), "queued_by_user": true}
 		if err := txn.Model(&report).Updates(updates).Error; err != nil {
 			slog.Error("error queueing stale author report for update", "author_report_id", report.Id, "error", err)
 			return ErrReportAccessFailed
@@ -72,7 +75,7 @@ func (r *ReportManager) queueAuthorReportUpdateIfNeeded(txn *gorm.DB, report *sc
 	return nil
 }
 
-func createOrGetAuthorReport(txn *gorm.DB, authorId, authorName, source string) (schema.AuthorReport, error) {
+func createOrGetAuthorReport(txn *gorm.DB, authorId, authorName, source string, fromUserReq bool) (schema.AuthorReport, error) {
 	var report schema.AuthorReport
 	result := txn.Clauses(clause.Locking{Strength: "UPDATE"}).Limit(1).Find(&report, "author_id = ? AND source = ?", authorId, source)
 	if result.Error != nil {
@@ -88,6 +91,8 @@ func createOrGetAuthorReport(txn *gorm.DB, authorId, authorName, source string) 
 			AuthorName:    authorName,
 			Source:        source,
 			Status:        schema.ReportQueued,
+			QueuedAt:      time.Now(),
+			QueuedByUser:  fromUserReq,
 		}
 
 		if err := txn.Create(&report).Error; err != nil {
@@ -103,7 +108,7 @@ func (r *ReportManager) CreateAuthorReport(licenseId, userId uuid.UUID, authorId
 	userReportId := uuid.New()
 
 	err := r.db.Transaction(func(txn *gorm.DB) error {
-		report, err := createOrGetAuthorReport(txn, authorId, authorName, source)
+		report, err := createOrGetAuthorReport(txn, authorId, authorName, source, true /*fromUserReq*/)
 		if err != nil {
 			return err
 		}
@@ -196,29 +201,41 @@ type ReportUpdateTask struct {
 	EndDate    time.Time
 }
 
-func (r *ReportManager) GetNextAuthorReport() (*ReportUpdateTask, error) {
-	found := false
+func findNextAuthorReport(txn *gorm.DB) (*schema.AuthorReport, error) {
 	var report schema.AuthorReport
-
-	err := r.db.Transaction(func(txn *gorm.DB) error {
+	for _, queuedByUser := range [2]bool{true, false} {
 		result := txn.Clauses(clause.Locking{Strength: "UPDATE"}).
 			Limit(1).Order("queued_at ASC").
-			Find(&report, "status = ?", schema.ReportQueued)
+			Find(&report, "status = ? AND queued_by_user = ?", schema.ReportQueued, queuedByUser)
 		if result.Error != nil {
 			slog.Error("error getting next author report from queue", "error", result.Error)
-			return ErrReportAccessFailed
+			return nil, ErrReportAccessFailed
 		}
 
-		if result.RowsAffected != 1 {
-			return nil
+		if result.RowsAffected == 1 {
+			return &report, nil
+		}
+	}
+	return nil, nil
+}
+
+func (r *ReportManager) GetNextAuthorReport() (*ReportUpdateTask, error) {
+	var report *schema.AuthorReport
+
+	err := r.db.Transaction(func(txn *gorm.DB) error {
+		var err error
+		report, err = findNextAuthorReport(txn)
+		if err != nil {
+			return err
 		}
 
-		if err := txn.Model(&report).Update("status", schema.ReportInProgress).Error; err != nil {
-			slog.Error("error updating author report status to in progress", "error", err)
-			return ErrReportAccessFailed
+		if report != nil {
+			if err := txn.Model(report).Update("status", schema.ReportInProgress).Error; err != nil {
+				slog.Error("error updating author report status to in progress", "error", err)
+				return ErrReportAccessFailed
+			}
 		}
 
-		found = true
 		return nil
 	})
 
@@ -226,7 +243,7 @@ func (r *ReportManager) GetNextAuthorReport() (*ReportUpdateTask, error) {
 		return nil, err
 	}
 
-	if found {
+	if report != nil {
 		return &ReportUpdateTask{
 			Id:         report.Id,
 			AuthorId:   report.AuthorId,
@@ -633,7 +650,7 @@ func (r *ReportManager) UpdateUniversityReport(id uuid.UUID, status string, upda
 			var authorReports []schema.AuthorReport
 
 			for _, author := range authors {
-				report, err := createOrGetAuthorReport(txn, author.AuthorId, author.AuthorName, author.Source)
+				report, err := createOrGetAuthorReport(txn, author.AuthorId, author.AuthorName, author.Source, false /*fromUserReq*/)
 				if err != nil {
 					slog.Error("error getting author report to add to university report", "author_id", author.AuthorId, "university_report_id", id, "error", err)
 					return err
