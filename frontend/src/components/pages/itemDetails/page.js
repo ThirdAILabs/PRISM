e1e4353cc9b9b790120d391cf65b2b33ebd2f78a
ashutoshTAI <ashutosh@thirdai.com>
2025-03-10 15:16:14 -0500

    Fix 'Filter by Timeline' button size/padding issue. (#107)

--- frontend/src/components/pages/itemDetails/page.js
+++ frontend/src/components/pages/itemDetails/page.js
@@ -901,9 +901,10 @@ const ItemDetails = () => {
                     backgroundColor: 'rgb(160, 160, 160)',
                     border: 'none',
                     color: 'white',
-                    width: '200px',
+                    width: '180px',
                     fontWeight: 'bold',
                     fontSize: '14px',
+                    marginRight: '10px',
                   }}
                 >
                   Filter by Timeline
@@ -912,11 +913,12 @@ const ItemDetails = () => {
                   <div
                     className="dropdown-menu show p-2"
                     style={{
-                      width: '200px',
+                      width: '180px',
                       backgroundColor: 'rgb(160, 160, 160)',
                       border: 'none',
                       right: 0,
                       marginTop: '5px',
+                      marginRight: '10px',
                       color: 'white',
                       fontWeight: 'bold',
                       fontSize: '14px',
