1bfb7f319e786c05397462511430f8e59a4f6cab
Nicholas Meisburger <nmeisburger@gmail.com>
2025-03-19 11:53:22 -0400

    fix frontend

--- frontend/src/components/pages/itemDetails/page.js
+++ frontend/src/components/pages/itemDetails/page.js
@@ -208,6 +208,8 @@ const ItemDetails = () => {
 
         if (!isMounted) return;
 
+        console.log('Report:', Content);
+
         setAuthorName(report.AuthorName);
         setReportContent(report.Content);
         setInitialReportContent(report.Content);
@@ -302,17 +304,17 @@ const ItemDetails = () => {
 
     const displayStart = startDate
       ? parseLocalDate(startDate).toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'short',
-          day: 'numeric',
-        })
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+      })
       : 'earliest';
     const displayEnd = endDate
       ? parseLocalDate(endDate).toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'short',
-          day: 'numeric',
-        })
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+      })
       : 'today';
 
     setFilterMessage(`${displayStart} - ${displayEnd}`);
@@ -356,11 +358,10 @@ const ItemDetails = () => {
                                   type="button"
                                   className={`btn ${value ? 'btn-outline-danger' : 'btn-outline-success'} btn-sm`}
                                   style={{ minWidth: '180px', textAlign: 'center' }}
-                                  title={`${innerKey}: ${
-                                    value
-                                      ? 'The author likely IS a primary recipient of this grant.'
-                                      : 'The author likely IS NOT a primary recipient of this grant.'
-                                  }`}
+                                  title={`${innerKey}: ${value
+                                    ? 'The author likely IS a primary recipient of this grant.'
+                                    : 'The author likely IS NOT a primary recipient of this grant.'
+                                    }`}
                                 >
                                   {innerKey}
                                   {/* : {value ? 'Yes' : 'No'} */}
@@ -670,9 +671,9 @@ const ItemDetails = () => {
           {flag.RawAcknowledgements?.map((item, index3) => {
             return <p key={index3}>{item}</p>;
           })}
-          <p>{}</p>
+          <p>{ }</p>
         </p>
-        {}
+        { }
 
         <div>{fundCodeTriangulation(flag, index)}</div>
       </div>
@@ -863,17 +864,23 @@ const ItemDetails = () => {
               })}
             </ul>
           </>
-          <strong>Potential affiliate(s)</strong>
-          <ul className="bulleted-list">
-            {flag.DocEntities.map((item, index2) => {
-              const key = `${index} ${index2}`;
-              return (
-                <li key={key}>
-                  <a>{item}</a>
-                </li>
-              );
-            })}
-          </ul>
+          {
+            flag.DocEntities && flag.DocEntities.length > 0 && (
+              <>
+                <strong>Potential affiliate(s)</strong>
+                <ul className="bulleted-list">
+                  {flag.DocEntities.map((item, index2) => {
+                    const key = `${index} ${index2}`;
+                    return (
+                      <li key={key}>
+                        <a>{item}</a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            )
+          }
         </p>
       </div>
     );
