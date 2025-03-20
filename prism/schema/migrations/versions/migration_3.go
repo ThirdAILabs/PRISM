16fbced917499caef2697c80f029bdb6a1acc8e2
Gautam Sharma <gautam@thirdai.com>
2025-03-20 14:41:52 +0530

    fix migrations

--- prism/schema/migrations/versions/migration_3.go
+++ prism/schema/migrations/versions/migration_3.go
@@ -5,22 +5,12 @@ import (
 	"fmt"
 	"prism/prism/api"
 	"prism/prism/schema"
-	"time"
 
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 func Migration3(db *gorm.DB) error {
-	type Feedback struct {
-		Id        uuid.UUID         `gorm:"type:uuid;primaryKey"`
-		UserId    uuid.UUID         `gorm:"type:uuid;not null;index"`
-		Flag      schema.AuthorFlag `gorm:"foreignKey:ReportId;constraint:OnDelete:CASCADE"`
-		Timestamp time.Time
-		Data      []byte
-	}
-
-	err := db.AutoMigrate(&Feedback{})
+	err := db.AutoMigrate(&schema.Feedback{})
 	if err != nil {
 		return fmt.Errorf("failed to migrate Feedback: %w", err)
 	}
@@ -48,14 +38,14 @@ func Migration3(db *gorm.DB) error {
 		if err := db.Model(&schema.AuthorFlag{}).
 			Where("report_id = ? AND flag_hash = ?", flag.ReportId, flag.FlagHash).
 			Update("data", updatedData).Error; err != nil {
-			return fmt.Errorf("failed to update flag data for flag hash %s: %w", flag.FlagHash, err)
+			return fmt.Errorf("failed to update flag data for report %s, flag hash %s: %w", flag.ReportId, flag.FlagHash, err)
 		}
 	}
 
 	return nil
 }
 
-func Roolback3(db *gorm.DB) error {
+func Rollback3(db *gorm.DB) error {
 	err := db.Migrator().DropTable("feedback")
 	if err != nil {
 		return fmt.Errorf("failed to drop table feedback: %w", err)
